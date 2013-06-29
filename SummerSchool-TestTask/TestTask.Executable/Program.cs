c21cdc16258ca23a1ea19a7bd4f5cb30319d423d
GrigoryewSergey <GrigoryewSergey@gmail.com>
2013-06-29 15:44:32 +0600

    Добавлена инфраструктура решения
    Добалены проекты:
     - для запускаемой сборки
     - для математической библиотеки
     - для unit-тестов

---
+++ SummerSchool-TestTask/TestTask.Executable/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestTask.Executable
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
